c9f0647e63d3140856539a94f42d3268619d8f99
kiran garrix <kirangarrix2019@gmail.com>
2021-08-09 12:23:14 +0530

    components created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import  "./app.scss"
+import Topbar from "./components/topbar/Topbar"
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Works from "./components/works/Works";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+     <Topbar/>
+     <div className="sections">
+     <Intro />
+     <Portfolio/>
+     <Works/>
+     <Testimonials />
+     <Contact />
+
+     </div>
     </div>
   );
 }
